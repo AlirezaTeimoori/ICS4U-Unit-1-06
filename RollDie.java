bca7a97f16ee1e2835ce3d618d01b2be565d7de0
alireza <alirezateimoori80@gmail.com>
2019-02-14 00:16:27 -0500

    fix: refine code and add comments

--- RollDie.java
+++ RollDie.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 //---------------------------------------
 //-- Created by:     Alireza Teimoori  --
@@ -12,12 +13,32 @@ public class RollDie{ // Intro class RollDie
 
     public static Random randomNumber = new Random(); // Bring in an instance of Random
 
-    public static int rollDie(int userChoice) { // Intro rollDie function. Eats an int, processes it, gives another int. :)
-        
-        int output = RollDie.randomNumber.nextInt(userChoice); // generate the random number up to "userChoice" and store it in "output"
-        return output; // sends out output
+    public static int rollDie(int userMin, int userMax) { // Intro rollDie function. Eats an int, processes it, gives another int. :)
+        int output = -1; // the initial value of output in case the app didn't go under the if statement
+
+        if (userMax > userMin){ // Makes sure there are no negative numbers
+            
+            output = RollDie.randomNumber.nextInt(userMax - userMin) + userMin; // generate the random between the two extremes and store it in "output"
+            return output + 1; // sends out (output + 1) to make sure there are no 0s
+
+        } else { // a type of error message:
+            System.out.println("Please be rational :| (Max > Min)");
+        }
+
+        return output;
     }
     public static void main(String[] args) {
-        System.out.println(rollDie(20));
+        Scanner scanner = new Scanner(System.in); // Intro Scanner instance ==> scanner
+
+        // Asking for and storing data:
+        System.out.println("Enter the min value: ***(if you don't have a min value please type 0)"); // lets the user choose a min value if they want
+        int uMin = scanner.nextInt();
+        System.out.println("Enter the max value:");
+        int uMax = scanner.nextInt();
+
+        // Process and output:
+        System.out.println(rollDie(uMin, uMax));
+
+        scanner.close(); // The IDE won't leave you alone if you don't close this :\
     }
 }
